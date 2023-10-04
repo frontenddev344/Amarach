5f5b24b74fda8e05cb44c7a8ab2b71c1e4ce3cea
Your Name <you@example.com>
2023-10-04 11:59:00 +0530

    ssd

--- js/index.js
+++ js/index.js
@@ -24,24 +24,24 @@ $(document).ready(function (e) {
 });
 
 // owl-carousel
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 40,
-    autoplay: true,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        600: {
-            items: 2,
-            nav: false
-        },
-        1000: {
-            items: 3,
-            nav: false,
-            loop: true
-        }
-    }
-})
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 40,
+//     autoplay: true,
+//     responsiveClass: true,
+//     responsive: {
+//         0: {
+//             items: 1,
+//             nav: false
+//         },
+//         600: {
+//             items: 2,
+//             nav: false
+//         },
+//         1000: {
+//             items: 3,
+//             nav: false,
+//             loop: true
+//         }
+//     }
+// })
